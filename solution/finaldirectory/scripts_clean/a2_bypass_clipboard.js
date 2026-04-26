98236d926407f85d6d0b6305b573927e3ae79a62
Shaked Perets <130215814+ShakedPeretz@users.noreply.github.com>
2026-04-26 18:17:51 +0300

    Delete solution/finaldirectory/scripts_clean/a2_bypass_clipboard.js

--- solution/finaldirectory/scripts_clean/a2_bypass_clipboard.js
+++
@@ -1,7 +0,0 @@
-Java.perform(function () {
-    const NB = Java.use("runtime.loading.NativeBridge");
-
-    NB.handleClipboard.implementation = function (bytes, len, encrypt) {
-        return bytes;
-    };
-});
